96d729b84495dd56591b6d4d3224d1d2dd70fb10
Instance01 <d97354675+aslezar@users.noreply.github.com>
2023-01-19 13:27:11 -0500

    19Jan2023

---
+++ LeetCode/19Jan23.cpp
@@ -0,0 +1,26 @@
+// 974. Subarray Sums Divisible by K
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+#define el cout << '\n';
+int subarraysDivByK(vector<int>& nums, int k) {
+            vector<int> prefix(k,0);
+            prefix[0]=1;
+            int sum=0;
+            int modsum=0;
+            for(int i=0;i<nums.size();i++)
+            {
+                modsum=(modsum+nums[i]%k +k)%k;
+                sum+=prefix[modsum];
+                prefix[modsum]++;
+            }
+            return sum;
+        }
+int main()
+{
+    vector<int> nums({4,5,0,-2,-3,1});
+    cout << subarraysDivByK(nums,5);
+    return 0;
+}
