cb22ef807a8546b1c915b2da9f6a2abbeb17c335
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-01-20 13:39:17 -0500

    20Jan2023

--- LeetCode/20Jan2023.cpp
+++ LeetCode/20Jan2023.cpp
@@ -1,28 +1,49 @@
-//491. Non-decreasing Subsequences
-class Solution {
-public:
-    void backtrack(vector<int>& nums,vector<int>& seq, set<vector<int>>& result,int index)
+// 491. Non-decreasing Subsequences
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+#define el cout << '\n';
+void backtrack(vector<int> &nums, vector<int> &seq, set<vector<int>> &result, int index)
+{
+    if (index >= nums.size())
     {
-        if(index>=nums.size())
+        if (seq.size() > 1)
         {
-            if(seq.size()>1)
-                {result.insert(seq);}
-            return;
+            result.insert(seq);
         }
-        if(seq.empty() || seq.back()<=nums[index])
-        {
-            seq.push_back(nums[index]);
-            backtrack(nums,seq,result,index+1);
-            seq.pop_back();
-        }
-        backtrack(nums,seq,result,index+1);
+        return;
     }
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
-
-        set<vector<int>> ans;
-        vector<int> seq;
-        backtrack(nums,seq,ans,0);
-
-        return vector(ans.begin(),ans.end());
+    if (seq.empty() || seq.back() <= nums[index])
+    {
+        seq.push_back(nums[index]);
+        backtrack(nums, seq, result, index + 1);
+        seq.pop_back();
     }
-};
+    backtrack(nums, seq, result, index + 1);
+}
+vector<vector<int>> findSubsequences(vector<int> &nums)
+{
+
+    set<vector<int>> ans;
+    vector<int> seq;
+    backtrack(nums, seq, ans, 0);
+
+    return vector(ans.begin(), ans.end());
+}
+int main()
+{
+    vector<int> nums({4, 6, 7, 7});
+    vector<vector<int>> ans(findSubsequences(nums));
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << '[';
+
+        for (int j = 0; j < ans[i].size(); j++)
+
+            cout << ans[i][j] << ',';
+        cout << '\b' << ']' << '\n';
+    }
+    return 0;
+}
