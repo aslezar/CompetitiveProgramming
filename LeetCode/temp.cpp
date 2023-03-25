c73bc44d2ebe718f7e81d4fcfa5ee379226452ad
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-03-25 12:49:00 +0530

    25Mar2023

--- LeetCode/temp.cpp
+++ LeetCode/temp.cpp
@@ -1,35 +1,11 @@
-#include <vector>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
-class Solution
+
+#define el << '\n'
+#define ws << ' '
+
+int main()
 {
-public:
-    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src, long long &nodes)
-    {
-        visited[src] = true;
-        nodes++;
-        for (auto &&i : adj[src])
-            if (!visited[i])
-                dfs(adj, visited, i, nodes);
-    }
-    long long countPairs(int n, vector<vector<int>> &edges)
-    {
-        vector<vector<int>> g(n);
-        for (auto &&i : edges)
-        {
-            g[i[0]].push_back(i[1]);
-            g[i[1]].push_back(i[0]);
-        }
-        long long ans = 0;
-        vector<bool> v(n, false);
-        for (int i = 0; i < n; i++)
-        {
-            long long nodes = 0;
-            if (!v[i])
-            {
-                dfs(g, v, i, nodes);
-                ans += nodes * (n - nodes);
-            }
-        }
-        return ans / 2;
-    }
-};
+    return 0;
+}
