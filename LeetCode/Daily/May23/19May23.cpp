815b09e37e198f3c667c28db1a9b2e73727cafeb
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-05-19 19:56:32 +0530

    19May2023

---
+++ LeetCode/Daily/May23/19May23.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define el << '\n'
+#define ws << ' '
+
+class Solution
+{
+public:
+    bool isBipartite(vector<vector<int>> &gr)
+    {
+        int n = gr.size();
+        vector<int> colour(n, 0);
+
+        for (int node = 0; node < n; node++)
+        {
+            if (colour[node] != 0)
+                continue;
+
+            queue<int> q;
+            q.push(node);
+            colour[node] = 1;
+
+            while (!q.empty())
+            {
+                int cur = q.front();
+                q.pop();
+
+                for (int ne : gr[cur])
+                {
+                    if (colour[ne] == 0)
+                    {
+                        colour[ne] = -colour[cur];
+                        q.push(ne);
+                    }
+                    else if (colour[ne] != -colour[cur])
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
+int main()
+{
+
+    return 0;
+}
