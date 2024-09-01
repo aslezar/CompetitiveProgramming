ec2f30ca907c29bd99ae62a7300903f6645f1ac2
aslezar <97354675+aslezar@users.noreply.github.com>
2024-09-01 23:33:29 +0530

    Refactor C++ solution for CodeForces Contest969

--- LeetCode/Weekly413/C.cpp
+++ LeetCode/Weekly413/C.cpp
@@ -1,117 +1,75 @@
+#ifdef LOCAL_RUN
+
+// https://leetcode.com/problems/select-cells-in-grid-with-maximum-score/description/
+
 #include <bits/stdc++.h>
 using namespace std;
+#include "debug.cpp"
 
-#include <debug.cpp>
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
 
-// bitmask
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sorta(a) sort(all(a))
+#define sortd(a) sort(rall(a))
+#define set_bits __builtin_popcount
 
-// class Solution
-// {
-// public:
-//     int helper(vector<pair<int, int>> &v, int i, string r)
-//     {
-//         if (i == v.size())
-//         {
-//             return 0;
-//         }
-//         if (r[v[i].second] == '1')
-//         {
-//             // row already selected
-//         }
-//         else
-//         {
-//             // row not selected
-//         }
-//     }
-//     int maxScore(vector<vector<int>> &grid)
-//     {
-//         int ans = 0;
-//         string r = "0000000000";
+#define F first
+#define S second
+#define PB push_back
+#define gcd __gcd
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define input(vec, n) FOR(i, 0, n - 1) std::cin >> vec[i];
 
-//         vector<pair<int, int>> v;
-//         for (int i = 0; i < grid.size(); i++)
-//         {
-//             for (int j = 0; j < grid[i].size(); j++)
-//             {
-//                 v.push_back({grid[i][j], i});
-//             }
-//         }
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vvi;
 
-//         return ans;
-//     }
-// };
+constexpr unsigned int mod = 1e9 + 7;
+constexpr unsigned int maxn = 1e5 + 4;
+constexpr float EPS = numeric_limits<float>::epsilon();
+constexpr ll INF = numeric_limits<ll>::max();
 
 class Solution
 {
 public:
+    // int helper(vi &grid, int i,)
+    // {
+    //     if (i == grid.size())
+    //         return 0;
+    // }
     int maxScore(vector<vector<int>> &grid)
     {
         int ans = 0;
-
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<pair<int, pair<int, int>>> v;
-        // push all values in vector
-        for (int i = 0; i < grid.size(); i++)
+        vi g;
+        FOR(i, 0, grid.size() - 1)
         {
-            for (int j = 0; j < grid[0].size(); j++)
+            FOR(j, 0, grid[i].size() - 1)
             {
-                v.push_back({grid[i][j], {i, j}});
+                g.PB(grid[i][j]);
             }
         }
-
-        auto comp = [&](pair<int, pair<int, int>> &a, pair<int, pair<int, int>> &b)
-        {
-            if(a.first==b.first){
-                int row1=a.second.first;
-                int row2=b.second.first;
-                int col1=a.second.second+1;
-                int col2=b.second.second+1;
-                if(col1==n)return true;
-                if(col2==n)return false;
-                return grid[row1][col1] < grid[row2][col1];
-            }
-            return a.first > b.first; };
-
-        sort(v.begin(), v.end(), comp);
-        debug(v);
-
-        set<int> visRow;
-        set<int> visValue;
-        for (int i = 0; i < v.size(); i++)
-        {
-            int val = v[i].first;
-            int row = v[i].second.first;
-            int col = v[i].second.second;
-            if (visValue.find(val) == visValue.end() && visRow.find(row) == visRow.end())
-            {
-                visRow.insert(row);
-                visValue.insert(val);
-                ans += val;
-                debug(val);
-            }
-        }
-
         return ans;
     }
 };
 
+#undef int
+
+#ifdef LOCAL_RUN
+
 int main()
 {
     Solution s;
-    vector<vector<int>> grid = {};
 
-    // [[1,2,3],[4,3,2],[1,1,1]]
-    grid = {{1, 2, 3}, {4, 3, 2}, {1, 1, 1}};
-    // cout << s.maxScore(grid)<<endl;
+    // cout << s.checkTwoChessboards("a1", "b2") << endl;
+    cout << endl;
 
-    //  [[8,7,6],[8,3,2]]
-    grid = {{8, 7, 6}, {8, 3, 2}};
-    // cout << s.maxScore(grid)<<endl;
-
-    // [[19,18],[1,14],[4,14]]
-    grid = {{19, 18}, {1, 14}, {4, 14}};
-    cout << s.maxScore(grid) << endl;
     return 0;
 }
+
+#endif
--- LeetCode/Weekly413/D.cpp
+++ LeetCode/Weekly413/D.cpp
@@ -1,50 +1,88 @@
+#ifdef LOCAL_RUN
+
 #include <bits/stdc++.h>
 using namespace std;
-
-#ifdef LOCAL_RUN
 #include "debug.cpp"
+
 #else
 #define debug(...)
 #define debugArr(...)
 #endif
 
-// #define int long long
-#define ll long long
+#define int long long
 #define all(a) (a).begin(), (a).end()
 #define rall(a) (a).rbegin(), (a).rend()
 #define sorta(a) sort(all(a))
 #define sortd(a) sort(rall(a))
 #define set_bits __builtin_popcount
+
+#define F first
+#define S second
+#define PB push_back
 #define gcd __gcd
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define input(vec, n) FOR(i, 0, n - 1) std::cin >> vec[i];
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vvi;
 
 constexpr unsigned int mod = 1e9 + 7;
-constexpr ll maxn = 1e5 + 4;
+constexpr unsigned int maxn = 1e5 + 4;
 constexpr float EPS = numeric_limits<float>::epsilon();
 constexpr ll INF = numeric_limits<ll>::max();
 
-void solve([[maybe_unused]] ll &_case_no)
+class Solution
 {
-    ll n = 0;
-    cin >> n;
-    cout << n << endl;
-}
+public:
+    vector<int> maximumSubarrayXor(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        int n = nums.size();
+        vector<vector<int>> dp2(n, vector<int>(n));
+        for (int i = 0; i < n; i++)
+        {
+            dp2[i][i] = nums[i];
+            for (int j = i + 1; j < n; j++)
+            {
+                dp2[i][j] = dp2[i][j - 1] ^ nums[j];
+            }
+        }
+        debug(dp2);
 
-#define int long long
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = 0; i < n; i++)
+            dp[i][i] = nums[i];
+
+        for (int len = 2; len <= n; len++)
+        {
+            for (int st = 0; st + len - 1 < n; st++)
+            {
+                int end = st + len - 1;
+                dp[st][end] = dp[st][end - 1] ^ dp[st + 1][end];
+            }
+        }
+        debug(dp);
+
+        return {};
+    }
+};
+
+#undef int
 
 #ifdef LOCAL_RUN
 
-int32_t main()
+int main()
 {
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
-    ll t = 1;
-    cin >> t;
-    for (ll i = 0; i < t; i++)
-    {
-        // cout << "Case #" << i + 1 << ": ";
-        solve(i);
-    }
+    Solution s;
+    // nums = [2,8,4,32,16,1], queries = [[0,2],[1,4],[0,5]]
+    vector<ll> nums = {2, 8, 4, 32, 16, 1};
+    // vector<ll> nums = {1, 2, 3, 4, 5};
+    vector<vector<ll>> queries = {{0, 2}, {1, 4}, {0, 5}};
+
+    vector<ll> ans = s.maximumSubarrayXor(nums, queries);
+    debug(ans);
+
     return 0;
 }
 
