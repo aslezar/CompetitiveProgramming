85714494d45990dc7f7e5685cc5c175371013b3a
aslezar <97354675+aslezar@users.noreply.github.com>
2024-09-25 22:46:59 +0530

    Codechef 153

---
+++ Codechef/153/A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_RUN
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sorta(a) sort(all(a))
+#define sortd(a) sort(rall(a))
+#define set_bits __builtin_popcount
+
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define input(vec, n) FOR(i, 0, n - 1) std::cin >> vec[i];
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+constexpr unsigned int mod = 1e9 + 7;
+
+void solve([[maybe_unused]] ll &_case_no)
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    bool ans = true;
+    if (a > b + c)
+        ans = false;
+    if (b > a + c)
+        ans = false;
+    if (c > a + b)
+        ans = false;
+
+    cout << (ans ? "Yes" : "No") << endl;
+}
+
+int32_t main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    for (ll i = 0; i < t; i++)
+    {
+        // cout << "Case #" << i + 1 << ": ";
+        solve(i);
+    }
+    return 0;
+}
---
+++ Codechef/153/C.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_RUN
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sorta(a) sort(all(a))
+#define sortd(a) sort(rall(a))
+#define set_bits __builtin_popcount
+
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define input(vec, n) FOR(i, 0, n - 1) std::cin >> vec[i];
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+constexpr unsigned int mod = 1e9 + 7;
+
+void solve([[maybe_unused]] ll &_case_no)
+{
+    ll n = 0;
+    cin >> n;
+
+    vii arr(n + 1);
+    for (size_t i = 0; i < n - 1; i++)
+    {
+        ll a, b;
+        cin >> a >> b;
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }
+    ll ans = 0;
+    for (size_t i = 1; i <= n; i++)
+    {
+        if (arr[i].size() == 1)
+        {
+            ans += 3;
+        }
+        else
+        {
+            ans += 2;
+        }
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    for (ll i = 0; i < t; i++)
+    {
+        // cout << "Case #" << i + 1 << ": ";
+        solve(i);
+    }
+    return 0;
+}
