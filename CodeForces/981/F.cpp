b11bcd747f667726770fc101f9ed3f32ba06f7a3
aslezar <97354675+aslezar@users.noreply.github.com>
2024-10-25 18:42:59 +0530

    Codeforces 981

---
+++ CodeForces/981/F.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_RUN
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sorta(a) sort(all(a))
+#define sortd(a) sort(rall(a))
+#define input(vec, n)           \
+    for (int i = 0; i < n; i++) \
+        std::cin >> vec[i];
+
+#define el << endl;
+#define ws << " ";
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+constexpr unsigned int mod = 1e9 + 7;
+
+void solve([[maybe_unused]] ll &_case_no)
+{
+    ll n = 0;
+    cin >> n;
+
+    ll k;
+    cin >> k;
+
+    if (k == 1)
+    {
+        cout << (n % mod) el;
+        return;
+    }
+
+    int a = 1, b = 1;
+    int index = 2;
+    while (b % k != 0)
+    {
+        int l = (a + b) % k;
+        a = b;
+        b = l;
+        index++;
+    }
+
+    n %= mod;
+    ll ans = (index * n) % mod;
+    cout << ans el;
+}
+
+int32_t main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    for (ll i = 0; i < t; i++)
+    {
+        // cout << "Case #" << i + 1 << ": ";
+        solve(i);
+    }
+    return 0;
+}
