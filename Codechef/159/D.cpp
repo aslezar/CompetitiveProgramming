57c59e8c6bed9c750e98a9f582b6a55ad5d6c5f0
aslezar <97354675+aslezar@users.noreply.github.com>
2024-11-11 23:40:05 +0530

    Add solution for Codechef problem 159 and update input test cases

---
+++ Codechef/159/D.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_RUN
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sorta(a) sort(all(a))
+#define sortd(a) sort(rall(a))
+#define input(vec, n)           \
+    for (int i = 0; i < n; i++) \
+        std::cin >> vec[i];
+
+#define el << endl;
+#define ws << " ";
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+constexpr unsigned int mod = 1e9 + 7;
+
+long long power_mod(long long base, long long exp, long long mod = 1e9 + 7)
+{
+    long long result = 1;
+    while (exp > 0)
+    {
+        if (exp % 2 == 1)
+            result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp = exp / 2;
+    }
+    return result;
+}
+
+void solve([[maybe_unused]] ll &_case_no)
+{
+    ll n = 0;
+    cin >> n;
+
+    ll d = 0;
+    cin >> d;
+
+    vi v(n);
+    input(v, n);
+
+    int ans = ((d + 1) * (power_mod(2 * d, mod - 2))) % mod;
+    cout << ans el;
+}
+
+int32_t main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    for (ll i = 0; i < t; i++)
+    {
+        // cout << "Case #" << i + 1 << ": ";
+        solve(i);
+    }
+    return 0;
+}
