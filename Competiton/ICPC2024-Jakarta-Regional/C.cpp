211bcb5bfca20ff09d92eff8815178af51da1448
aslezar <97354675+aslezar@users.noreply.github.com>
2024-12-01 15:33:01 +0530

    Add ICPC 2024 solutions and update build configuration

---
+++ Competiton/ICPC2024-Jakarta-Regional/C.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_RUN
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sorta(a) sort(all(a))
+#define sortd(a) sort(rall(a))
+#define input(vec, n)           \
+    for (int i = 0; i < n; i++) \
+        std::cin >> vec[i];
+
+#define el << endl;
+#define ws << " ";
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+constexpr unsigned int mod = 1e9 + 7;
+
+void solve([[maybe_unused]] ll &_case_no)
+{
+    string s, t;
+    cin >> s >> t;
+
+    unordered_map<char, int> mp;
+    for (int i = 0; i < t.length(); i++)
+    {
+        mp[t[i]] = i;
+    }
+    int ans = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (mp.count(s[i]) == 0)
+        {
+            continue;
+        }
+        // ans = min(ans, i+1mp[s[i]]);
+    }
+}
+
+int32_t main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    for (ll i = 0; i < t; i++)
+    {
+        // cout << "Case #" << i + 1 << ": ";
+        solve(i);
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 To make this path available for all future g++ invocations, you can set the CPLUS_INCLUDE_PATH environment variable:
 export CPLUS_INCLUDE_PATH=/path/to/headers:$CPLUS_INCLUDE_PATH
+
+
+## Use this to pre-compile bits/header file
+# use different flags if running with differnet flags
+# -Winvalid-pch will tell if pre-compiler header file is used or not
+sudo g++ -DLOCAL_RUN -I/media/histox/LinuxFiles/Files/CompetitiveProgramming -O2 -Wall -Wconversion -Winvalid-pch -fdiagnostics-color=always -fsanitize=address -g -std=c++20 /usr/include/x86_64-linux-gnu/c++/13/bits/stdc++.h
