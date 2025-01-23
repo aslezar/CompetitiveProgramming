2b4537473ab37d276c435b5423a59226943bb1c8
aslezar <97354675+aslezar@users.noreply.github.com>
2025-01-23 13:26:47 +0530

    Codeforces Problem 1085B

---
+++ CodeForces/Problemset/1085B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_RUN
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sorta(a) sort(all(a))
+#define sortd(a) sort(rall(a))
+#define input(vec, n)           \
+    for (int i = 0; i < n; i++) \
+        std::cin >> vec[i];
+
+#define el << endl;
+#define ws << " ";
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+typedef pair<int, int> pii;
+constexpr unsigned int mod = 1e9 + 7;
+
+void solve([[maybe_unused]] ll &_case_no)
+{
+    int n, k;
+    cin >> n >> k;
+
+    int ans = INT_MAX;
+    for (int i = 1; i < k; i++)
+    {
+        if (n % i == 0)
+        {
+            int x = (n / i) * k + i;
+            ans = min(ans, x);
+        }
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+    ll t = 1;
+    for (ll i = 0; i < t; i++)
+    {
+        // cout << "Case #" << i + 1 << ": ";
+        solve(i);
+    }
+    return 0;
+}
